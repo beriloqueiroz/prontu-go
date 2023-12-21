669ee3584c05df31ff2f44fc6708dcc2a4b3ba0d
berilo queiroz <luiz.berilo@buson.com.br>
2023-12-21 17:51:33 -0300

    criando middleware

--- internal/infrastructure/api/routes/routes.go
+++ internal/infrastructure/api/routes/routes.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	controllers "github.com/beriloqueiroz/prontu-go/internal/infrastructure/api/controller"
+	middleware "github.com/beriloqueiroz/prontu-go/internal/infrastructure/api/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/api/sessions", controllers.GetSessions).Methods("GET")
 	r.HandleFunc("/api/sessions/{id}", controllers.GetSessionById).Methods("GET")
 	r.HandleFunc("/api/sessions/{id}", controllers.DeleteSession).Methods("DELETE")
